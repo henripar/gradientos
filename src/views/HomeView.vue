04a3fd76de92d1d0ed067359bc72fc24c97b09d5
Henri Parviainen <henri.parviainen@icloud.com>
2022-07-17 00:34:52 +0300

    Added Github link

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,27 @@
 <template>
   <div :class="['mainContainer', darkmode ? 'darkBG' : 'lightBG']">
-    <DarkModeButton
-      class="darkModeBtnContainer"
-      :darkmode="darkmode"
-      @dark-mode-switch="changeDarkMode"
-    />
+    <div class="darkModeBtnContainer">
+      <a href="https://www.github.com/henripar/gradientos">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          :style="darkmode ? { color: '#fff' } : { color: '#000' }"
+        >
+          <path
+            d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"
+          ></path>
+          <path d="M9 18c-4.51 2-5-2-7-2"></path>
+        </svg>
+      </a>
+      <DarkModeButton :darkmode="darkmode" @dark-mode-switch="changeDarkMode" />
+    </div>
     <Header
       :darkmode="darkmode"
       text="Hand-picked CSS gradients on UI elements."
@@ -119,6 +136,13 @@ export default {
   position: absolute;
   top: 50px;
   right: 10%;
+  display: flex;
+  align-items: baseline;
+  width: 80px;
+  justify-content: space-between;
+}
+.darkModeBtnContainer > .light,
+.dark {
   cursor: pointer;
 }
 
