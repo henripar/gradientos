2462ada6e0f3bf5a2e06f90f1c7b28ab3621861b
Henri Parviainen <henri.parviainen@icloud.com>
2022-07-17 15:13:06 +0300

    Replaced createWebHashHistory with createWebHistory in router to remove hashtags from urls

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 
 const routes = [
@@ -20,7 +20,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     // always scroll to top
